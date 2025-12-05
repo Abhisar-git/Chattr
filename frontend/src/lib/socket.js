df66627443253cea05f00a167a88f0d851c51425
Abhisar Prince Jha <abhisarprincejha@gmail.com>
2025-12-05 20:02:17 +0530

    backend url changes for production environment

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+  "https://chattr-itmd.onrender.com/";
 
 let socket = null;
 
